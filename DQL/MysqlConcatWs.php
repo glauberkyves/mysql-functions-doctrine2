3f1d1dd9941e6a96dc847801860f9364f169d37d
Glauber Oliveira <gkyves@gmail.com>
2015-02-07 14:19:58 -0200

    Create function concatws for doctrine2

---
+++ DQL/MysqlConcatWs.php
@@ -0,0 +1,61 @@
+<?php
+namespace GlauberKyves\MysqlDoctrineFunctions\DQL;
+
+use \Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use \Doctrine\ORM\Query\Lexer;
+use \Doctrine\ORM\Query\SqlWalker;
+use \Doctrine\ORM\Query\Parser;
+
+/**
+ * MysqlConcatWs
+ *
+ * @uses FunctionNode
+ * @author Glauber Kyves <glauberkyves@gmail.com>
+ */
+class MysqlConcatWs extends FunctionNode
+{
+
+    public $firstStringPrimary;
+
+    public $secondStringPrimary;
+
+    public $concatExpressions = array();
+
+    /**
+     * @param SqlWalker $sqlWalker
+     * @return string
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        $args = array();
+        foreach ($this->concatExpressions as $expression) {
+            $args[] = $sqlWalker->walkStringPrimary($expression);
+        }
+
+        return 'CONCAT_WS(' . join(', ', (array)$args) . ')';
+    }
+
+    /**
+     * @param Parser $parser
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->firstStringPrimary  = $parser->StringPrimary();
+        $this->concatExpressions[] = $this->firstStringPrimary;
+
+        $parser->match(Lexer::T_COMMA);
+
+        $this->secondStringPrimary = $parser->StringPrimary();
+        $this->concatExpressions[] = $this->secondStringPrimary;
+
+        while ($parser->getLexer()->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->concatExpressions[] = $parser->StringPrimary();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# mysql-functions-doctrine2
-Functions Mysql for Doctrine2
+MySQL functions Doctrine2
+====================
+
+This library provides you MySQL functions for Doctrine2.
+
+At the moment are supported
+
+ - CONCAT_WS
+
+Feel free to fork and add other functions.
+
+## Installation
+
+### Get the bundle
+
+Add this in your composer.json
+
+```json
+{
+	"require": {
+		"glauberkyves/mysql-functions-doctrine2": "1.*"
+	}
+}
+```
+
+and then run
+
+```sh
+php composer.phar update
+```
+or 
+```sh
+composer update
+```
+if you installed composer globally.
+
+### Add the classes to your configuration
+
+```php
+$config = new \Doctrine\ORM\Configuration();
+$config->addCustomStringFunction('concat_ws', 'GlauberKyves\MysqlDoctrineFunctions\DQL\MysqlRand');
+
+$em = EntityManager::create($dbParams, $config);
+```
+You can of course pick just the functions you need.
+
+### Use with Symfony2
+If you install the library in a Symfony2 application, you can add this in your config.yml
+
+```yaml
+# app/config/config.yml
+doctrine:
+    orm:
+        # ...
+        entity_managers:
+            default:
+                # ...
+                dql:
+                    string_functions:
+                        concat_ws:        GlauberKyves\MysqlDoctrineFunctions\DQL\MysqlConcatWs
+```
+
+### Usage
+You can now use the functions in your DQL Query
+
+```php
+$query = 'SELECT CONCAT_WS('string', 'string')
+        FROM ...
+    ';
+$em->createQuery($query);
+
+```
